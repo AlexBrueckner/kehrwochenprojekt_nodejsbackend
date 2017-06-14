c886df8057de0d64e2481fd31a7a28d609588892
MatthiasHofmaier <matthias.hofmaier@student.reutlingen-university.de>
2017-06-14 20:53:10 +0200

    asdas
    Asdasd

--- dbfunctions.js
+++ dbfunctions.js
@@ -78,14 +78,14 @@ exports.deleteUser = function(userName) {
 // db function to delete a Flat with given flatid
 exports.deleteUserFromFlat = function(userName, flatId) {
   User.findOneAndRemove({"userName":userName}, function(err, user){
-    if(!err){
+  /*  if(!err){
     console.log("User with name: " + userName + " successfuly removed");
     Flat.findOneAndRemove({"residents.user._id":user._id}, function(err){
       if (!err) {
         console.log("User with name:" + userName + " successfuly removed from FLAT");
       }
     });
-  }
+  }*/
   });
 }
 // =============================================================================
