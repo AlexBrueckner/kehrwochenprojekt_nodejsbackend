5c3ccfa296ce8b55db2fb659d3eac5503acc9afa
MatthiasHofmaier <matthias.hofmaier@student.reutlingen-university.de>
2017-07-04 22:14:14 +0200

    forgot to change method name

--- dbfunctions.js
+++ dbfunctions.js
@@ -568,7 +568,7 @@ exports.getUserByUserName = function(res, query) {
 
 // =============================================================================
 // db function to get an User with given userName
-exports.getUserByUserName = function(res, query) {
+exports.getUserByUserId = function(res, query) {
   User.findOne({
     "_id": query.userId
   }, function(err, user) {
